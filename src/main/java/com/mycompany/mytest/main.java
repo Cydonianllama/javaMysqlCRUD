5097b39d6dfc5f5112d8d65d066877c3f077d512
CydonianLlama <142017erick@gmail.com>
2021-04-02 22:20:54 -0500

    CRUD order finalized

--- src/main/java/com/mycompany/mytest/main.java
+++ src/main/java/com/mycompany/mytest/main.java
@@ -1,9 +1,11 @@
 package com.mycompany.mytest;
 
 import com.mycompany.mytest.entities.product.ProductRepository;
+import com.mycompany.mytest.entities.order.OrderRepository;
 import com.mycompany.mytest.utils.ConnectionDB;
 
 import com.mycompany.mytest.models.Product;
+import com.mycompany.mytest.models.Order;
 
 /**
  *
@@ -11,11 +13,13 @@ import com.mycompany.mytest.models.Product;
  */
 public class main {
     public static void main(String[] args){
-        ProductRepository productRepo = new ProductRepository(new ConnectionDB());
-        //System.out.println(productRepo.findAllProducts().get(0).getIdProduct());
-        
-        Product product = new Product("product-2","uva","fruta",Float.parseFloat("15.5"));
-        System.out.println(productRepo.update(product));
+        ConnectionDB DB = new ConnectionDB();  
+        ProductRepository productRepo = new ProductRepository(DB);
+        OrderRepository orderRepo = new OrderRepository(DB);
+        Order o_ = new Order(
+                "order-2",
+                "cashier-1",
+                "finalized");
 
     } 
 }
